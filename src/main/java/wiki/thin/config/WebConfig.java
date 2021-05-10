cd1631e49e1cd4586a985f53d3f77614d571fa7a
beldon <1047325205@qq.com>
2021-05-10 23:44:34 +0800

    prepare jobs for templates

--- src/main/java/wiki/thin/config/WebConfig.java
+++ src/main/java/wiki/thin/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer, ServletContextInitializer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("file:./templates/", "classpath:/templates/");
+        registry.addResourceHandler("/theme/**").addResourceLocations("file:./theme/", "classpath:/theme/");
         registry.addResourceHandler("/static/**").addResourceLocations("file:./static/", "classpath:/templates/static/");
     }
 
