38c62626fbaefbc976e2cd3a22668963c7883aea
beldon <1047325205@qq.com>
2021-11-27 16:09:02 +0800

    clean code

--- src/main/java/wiki/thin/theme/ftlh/ThemeFreemarkerView.java
+++
@@ -1,9 +0,0 @@
-package wiki.thin.theme.ftlh;
-
-import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
-
-/**
- * @author Beldon
- */
-public class ThemeFreemarkerView extends FreeMarkerView {
-}
